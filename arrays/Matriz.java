70240120960f25d5a67904fbb42deb3b611f9bb5
wellerson <wellersonjava@gmail.com>
2022-12-07 14:42:48 -0300

    Melhorando a classe matriz

--- arrays/Matriz.java
+++ arrays/Matriz.java
@@ -14,7 +14,18 @@ public class Matriz {
         int quantidadeNotas = entrada.nextInt();
 
         double[][] notasDaTurma = new double[alunos][quantidadeNotas];
+        double total = 0;
+        for(int a = 0; a < notasDaTurma.length; a++){
+            for (int n = 0; n < notasDaTurma[a].length; n++) {
+                System.out.printf("Informe a nota %d do aluno %d: "
+                , n + 1, a + 1);
+                notasDaTurma[a][n] = entrada.nextDouble();
+                total += notasDaTurma[a][n];
 
+            }
+        }
+        double media = total / (alunos * quantidadeNotas);
+        System.out.println("Média da turma é "+ media);
         entrada.close();
     }
 }
