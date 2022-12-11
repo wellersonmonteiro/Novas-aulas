69af83f4fffa352667c07132bbcd5f74933366fb
wellerson <wellersonjava@gmail.com>
2022-12-10 23:26:43 -0300

    Adição de Conjunto Compartado.

--- colecoes/ConjuntoCompartado.java
+++ colecoes/ConjuntoCompartado.java
@@ -1,4 +1,19 @@
 package colecoes;
 
+import java.util.*;
+
 public class ConjuntoCompartado {
+    public static void main(String[] args) {
+
+        SortedSet<String> lista = new TreeSet<>();
+        lista.add("Ana");
+        lista.add("Carlos");
+        lista.add("Luca");
+        lista.add("Pedro");
+        for (String canditado: lista){
+            System.out.println(canditado);
+        }
+    }
+
+
 }
